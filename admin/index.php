ee569a3640d6727446ab4c545c7a67eb4328fdf8
muhammad <muhammad.suwanda@jerbeeindonesia.com>
2013-06-10 09:49:48 +0700

    add folder admin

---
+++ admin/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SMK Al-Ittihad</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="http://twitter.github.io/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png">
+<link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
+<link href="css/docs.css" rel="stylesheet" type="text/css">
+</head>
+<body background="img/bg.png">
+<?php //================================================ ?>
+<div id="navbar-example" class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+          <div class="container" style="width:auto;">
+                  <a class="brand" href="#">SMK Al-Ittihad &nbsp;</a>
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            		<span class="icon-bar"></span>
+            		<span class="icon-bar"></span>
+            		<span class="icon-bar"></span>
+          		</button>
+          <div class="nav-collapse collapse">
+                  <ul class="nav" >
+                      
+                    <li class="dropdown">
+                      <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                      <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="http://google.com/">Action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#anotherAction">Another action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                      </ul>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" id="drop2" role="button" class="dropdown-toggle" data-toggle="dropdown">Dropdown 2 <b class="caret"></b></a>
+                      <ul class="dropdown-menu" role="menu" aria-labelledby="drop2">
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                        <li role="presentation" class="divider"></li>
+                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+<div class="container">
+<?php //======================================================= ?>
+  <div class="well-small">  
+    <div id="myCarousel" class="carousel slide">
+    <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <!-- Carousel items -->
+    <div class="carousel-inner">
+    <div class="active item"><img src="images/1.png">
+      <div class="carousel-caption">
+        <h4>Third Thumbnail label</h4>
+        <p>Cras justo odio, id dolor id nibh ultricies vehicula ut id elit.</p>
+      </div>
+    </div>
+    <div class="item"><img src="img/slide.png">
+      <div class="carousel-caption">
+          <h4>Third Thumbnail label</h4>
+          <p>Cras justo odio, id dolor id nibh ultricies vehicula ut id elit.</p>
+      </div>
+    </div>
+    <div class="item"><img src="img/slide.png">
+      <div class="carousel-caption">
+        <h4>Third Thumbnail label</h4>
+        <p>Cras justo odio, id dolor id nibh ultricies vehicula ut id elit.</p>
+      </div>
+    </div>
+    </div>
+    <!-- Carousel nav -->
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div>
+  </div>
+<?php //======================================================== ?>
+    <div class="row">
+    <div class="span3">
+      <div class="well">
+      <h4>Sidebar nav</h4>
+        <table border="" class="table table-bordered">
+        	<tr>
+            	<td>liat</td
+            ></tr>
+            <tr>
+				<td>liat</td>
+            </tr>
+        </table>
+      </div>
+    </div>
+    <div class="span6">
+      <div class="well">
+        <h3>Judul Posting</h3>
+        <img src="1.png" class="img-polaroid" style="float:left;margin-right:5px;">
+         &nbsp; &nbsp; &nbsp;Nnnnmofafm fmakfmkafm gamkgmakgm gmakgmakmg gamgkamgka 
+        fakfakfmka fmkafmkamk fmkamfkamf fmakfmakfm fakfknfakn <a href="#"><span class="label label-success">read more</span></a>
+      </div>
+    </div>
+    <div class="span3" >
+      <div class="well">
+      <h4>Sidebar nav</h4>
+        fafmkam fkmaf fiamfakf fmamfk mfka fmakfmakm fkamfkmak fkmafkmakf fkamfkm
+      </div>
+    </div>
+    </div>
+</div>
+
+<footer class="footer">
+      <div class="container">
+        <p>&copy; 2013 Design by <a href="http://facebook.com/muhammad.suwanda">Muhammad Suwanda</a> , SMK Al-Ittihad Cianjur</p>
+      </div>
+    </footer>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
